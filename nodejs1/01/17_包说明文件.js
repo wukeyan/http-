1746ae4f1198672a3f3b0c19102366fe66eef5e0
wukeyan <1946376552@qq.com>
2020-01-10 21:19:52 +0800

    我的node.js

---
+++ nodejs1/01/17_包说明文件.js
@@ -0,0 +1,7 @@
+// npm : node package manager包管理器
+// npm 有官网上面可以搜索下载各种包，只要上面搜得到就可以npm install --save 包名
+//package.json文件，就像产品说明书一样，这个文件可以用命令行 npm init
+//npm install --save 包名   只有通过--sava下载的包才会被package.json文件保的依赖项dependencies保存，一般一定要带--sava
+// npm nuinstall 包名
+//npm help查看帮助,如忘记uninstall了就可以写 npm uninstall --help来查找
+
